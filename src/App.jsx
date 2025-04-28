edfe39e06f09832988e15ea2ef8a9a54595696ec
AdwaithGit <adwaithmadhu12@gmail.com>
2025-04-28 12:29:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+// src/App.jsx
+import React from "react";
+import Navbar from "./Components/Navbar"; // Import Navbar component
+import "./App.css"; // Optional global styles (if any)
+import IrrigationTable from "./Components/IrrigationTable";
+import IrrigationChart from "./Components/IrrigationChart";
+
+
+const data = [
+  { index: 0, plot: 'D1', startTime: '070000', endTime: '070459', RunBy: 'M1' },
+  { index: 1, plot: 'D2', startTime: '070000', endTime: '070459', RunBy: 'M2' },
+  { index: 2, plot: 'D3', startTime: '070500', endTime: '070959', RunBy: 'M1' },
+  { index: 3, plot: 'D4', startTime: '070500', endTime: '070959', RunBy: 'M2' },
+  { index: 4, plot: 'D1', startTime: '073000', endTime: '073459', RunBy: 'M1' },
+  { index: 5, plot: 'D2', startTime: '073000', endTime: '073459', RunBy: 'M2' },
+  { index: 6, plot: 'D3', startTime: '073500', endTime: '073959', RunBy: 'M1' },
+  { index: 7, plot: 'D4', startTime: '073500', endTime: '073959', RunBy: 'M2' },
+  // Add more entries here...
+];
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <IrrigationTable />
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../Styles/Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <h2>Irrigation System</h2>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
