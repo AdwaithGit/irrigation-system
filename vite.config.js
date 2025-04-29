b9f68dea6c584f38e33f0dd67c432105f1bf4da5
AdwaithGit <adwaithmadhu12@gmail.com>
2025-04-29 11:45:51 +0530

    changed vite.config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-  // https://vite.dev/config/
-  export default defineConfig({
-    plugins: [react()],
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
 
-    build: {
-      outDir: 'build', // Make sure this is the directory that you want to deploy from
-    },
-  })
+
+})
